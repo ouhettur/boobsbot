feab3827675f6e7b3126070f04c1b385d1988227
Sergey Andrianov <asx926456@gmail.com>
2019-01-11 18:30:36 +0300

    1.2.5 add config

---
+++ config.py
@@ -0,0 +1,20 @@
+logindb = 'logindb'
+passdb = 'passdb'
+dbhost = 'dbhost'
+dbname = 'dbname'
+token = 'token'
+
+ACCESS_KEY_ID = 'ACCESS_KEY_ID'
+ACCESS_SECRET_KEY = 'ACCESS_SECRET_KEY'
+BUCKET_NAME = 'BUCKET_NAME'
+
+try:
+    from dev_settings import logindb, passdb, dbhost, dbname, token, ACCESS_KEY_ID, ACCESS_SECRET_KEY, BUCKET_NAME
+except ImportError:
+    print('no file dev_settings.py in the directory')
+    try:
+        from prod_settings import logindb, passdb, dbhost, dbname, token, ACCESS_KEY_ID, ACCESS_SECRET_KEY, BUCKET_NAME
+    except ImportError:
+        raise ImportError('no file dev_settings.py or prod_settings.py in the directory')
+
+
