f82d80a9b354ba18ee5591e8388b7b01eaf38a67
Sergey Andrianov <asx926456@gmail.com>
2019-01-13 02:58:58 +0300

    1.2.8
    fix id inline result

--- bot.py
+++ bot.py
@@ -214,11 +214,11 @@ def inlinequery(bot, update):
     for media in find_random_media(count=7):
         if media.media_type == 'img':
             results.append(InlineQueryResultCachedPhoto(type='photo',
-                                                        id=update.inline_query.id,
+                                                        id=uuid.uuid4(),
                                                         photo_file_id=media.telegram_file_id))
         elif media.media_type == 'animation':
             results.append(InlineQueryResultCachedMpeg4Gif(type='mpeg4_gif',
-                                                           id=update.inline_query.id,
+                                                           id=uuid.uuid4(),
                                                            mpeg4_file_id=media.telegram_file_id))
 
     update.inline_query.answer(results, next_offset=7, cache_time=0)
